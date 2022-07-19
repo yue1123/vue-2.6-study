2f30d66c00fabe1cd0e9fc1cecd84fb9623591d7
花开半亩地 <2572468699@qq.com>
2022-07-20 00:48:39 +0800

    feat: 理解 targetStack

--- src/core/observer/dep.js
+++ src/core/observer/dep.js
@@ -51,17 +51,20 @@ export default class Dep {
 
 // 这是一个动态的值, 全局只有一个,因为 js是单线程, 同一时间只能初始化一个组件 Watcher
 // FIXME: 模版里面有子组件的情况怎样处理??
+// 答案就是用一个栈来存起来
+// 当页面中有子组件时,全局的 watcher (也就是 Dep.target)会一直变,子组件 Watcher 会覆盖父级组件 Watcher,所以用一个栈来存起来,子组件完了能恢复父组件的 Watcher 引用
 // The current target watcher being evaluated.
 // This is globally unique because only one watcher
 // can be evaluated at a time.
 Dep.target = null
 const targetStack = []
-
+// 往栈里面放一个 Watcher
 export function pushTarget (target: ?Watcher) {
   targetStack.push(target)
   Dep.target = target
 }
 
+// 弹出一个栈尾元素,然后把 stack 的最后一个元素赋给 Dep.target, 恢复上一个,变更前的 Dep.target
 export function popTarget () {
   targetStack.pop()
   Dep.target = targetStack[targetStack.length - 1]
